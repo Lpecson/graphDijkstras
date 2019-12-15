f589187bc37552f2ac0a5a5f50e21cbee67a3ea5
Lpecson <54653886+Lpecson@users.noreply.github.com>
2019-12-14 20:44:41 -0800

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -6,13 +6,28 @@
 #include"graph.h"
 #include<limits.h>
 using std::map;
+#include<iostream>
+using namespace std;
 Graph::Graph()
 {
 	
 }
 Graph::~Graph()
 {
-	
+	map<string, Vertex>:: iterator it;
+	for( it = vertices.begin(); it != vertices.end(); it++)
+	{
+		vertices[it->first].~Vertex();
+	}
+	map<string, vector<Neighbor>>:: iterator it2;
+	for( it2 = adjList.begin(); it2 != adjList.end(); it2++)
+	{
+		int size = adjList[it2->first].size();
+		for( int i=0; i<size; i++ )
+		{
+			adjList[it2->first][i].~Neighbor();
+		}
+	}
 }
 Graph::Vertex::Vertex()
 {
@@ -38,27 +53,49 @@ void Graph::addVertex(string name)
 }
 void Graph::addEdge( string from, string to, int weight)
 {
-	Neighbor* add = new Neighbor(to, weight);
-	Neighbor* add2 = new Neighbor(from, weight);
-	if( !adjList[from].empty() )
+	Neighbor fromAdd = Neighbor(to, weight);//here
+	Neighbor toAdd = Neighbor(from, weight);
+	bool fromExists = false;
+	bool toExists = false;
+	if( !adjList.empty() )
 	{
-		adjList[from].push_back(*add);
+		map<string, vector<Neighbor>>:: iterator it;
+		for( it = adjList.begin(); it != adjList.end(); it++)
+		{
+			if( it->first == from )
+			{
+				fromExists = true;
+			}
+			if( it->first == to )
+			{
+				toExists = true;
+			}
+		}
+		if(fromExists)
+		{
+			adjList[from].push_back(fromAdd);
+		}
+		else
+		{
+			vector<Neighbor> add = {fromAdd};
+			adjList[from] = add;
+		}
+		if(toExists)
+		{
+			adjList[to].push_back(toAdd);
+		}
+		else
+		{
+			vector<Neighbor> add = {toAdd};
+			adjList[to] = add;
+		}
 	}
 	else
 	{
-		vector<Neighbor> temp;
-		temp.push_back(*add);
-		adjList.insert(std::pair<string, vector<Neighbor>>(from, temp));		
-	}
-	if( !adjList[to].empty() )
-	{
-		adjList[to].push_back(*add2);
-	}
-	else
-	{
-		vector<Neighbor> temp2;
-		temp2.push_back(*add2);
-		adjList.insert(std::pair<string, vector<Neighbor>>(to, temp2));
+		vector<Neighbor> add = {fromAdd};
+		vector<Neighbor> add2 = {toAdd};
+		adjList[from] = add;
+		adjList[to] = add2;
 	}
 }
 void Graph::buildSSPTree(string source)
